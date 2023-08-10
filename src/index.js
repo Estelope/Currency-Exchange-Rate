857ed4228d067652282aee863e56405fc1e53c43
Estelope <elopez14567@gmail.com>
2023-08-10 14:09:41 -0600

    add instance of class constructor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyExchangeRate from './otherScripts';
+
+//business 
+async function getConversionRate(currency) {
+  const response = await CurrencyExchangeRate.getConversionRate(currency);
+  if (response.main) {
+    printElements(response, currency);
+  } else {
+    printError(response, currency);
+  }
+}
+
+
 //ui
 
 window.addEventListener("load", function() {
