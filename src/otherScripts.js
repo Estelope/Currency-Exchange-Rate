14b9fec416b18407e2ced74c7ec04890d27399b4
Estelope <elopez14567@gmail.com>
2023-08-10 16:11:03 -0600

    remove old error handling

--- src/otherScripts.js
+++ src/otherScripts.js
@@ -4,14 +4,9 @@ export default class CurrencyExchangeRate {
     try {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}`);
       const jsonifiedResponse = await response.json();
-      if (!response.ok) {
-        let errorMessage = `${response.status} ${response.statusText}
-      ${jsonifiedResponse.message}`;
-        throw new Error(errorMessage);
-      }
       return jsonifiedResponse;
-    } catch (error) {
-      return error;  
+    } catch (error) { //throw is implied
+      return error;
     }
   }
 }
